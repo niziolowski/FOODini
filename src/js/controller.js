225bb7993711474143bc72556d970de49bbcddf4
niziolowski <krystianpiatkowski92@gmail.com>
2022-09-11 19:07:41 +0200

    Added logic to the theme-editor

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,59 @@
 import settingsView from "./views/settingsView.js";
 import shoppingListView from "./views/shoppingListView.js";
+import navView from "./views/navView.js";
+import view from "./views/view.js";
+import * as model from "./model.js";
+// § NAV
+
+function controlNav(e) {
+  // Get button
+  const btn = e.target.closest("button");
+
+  if (!btn) return;
+
+  // Settings button
+  if (btn.classList.contains("main-view__nav__btn-settings"))
+    settingsView.show();
+}
+
+// § SETTIGNS
+function controlSettings(e) {
+  // Handle overlay click
+  if (e.target.classList.contains("overlay")) settingsView.hide();
+  // Get clicked button
+  const btn = e.target.closest("button");
+  const input = e.target.closest("input");
+
+  // HANDLE COLOR THEMES
+  if (input) {
+    // Create a one time event handler
+    function handleColorThemes(e) {
+      // 1. update state
+      model.updateColorTheme(e.target);
+      // 2. update view
+      settingsView.updateColorTheme(model.state.colorTheme);
+
+      // Remove event listener (this)
+      input.removeEventListener("change", handleColorThemes);
+    }
+
+    input.addEventListener("change", handleColorThemes); // it will remove itself after executing
+  }
+}
 
 function init() {
+  // 1. Load
+
+  // load color-theme from state
+  settingsView.updateColorTheme(model.state.colorTheme);
+
   shoppingListView.init();
+  navView.addHandlerClick(controlNav);
+  settingsView.addHandlerClick(controlSettings);
 }
 
 init();
-// settingsView.show();
+
+// testing
+
+settingsView.show();
---
+++ src/js/model.js
@@ -0,0 +1,19 @@
+// § STATE
+export const state = {
+  colorTheme: [
+    { property: "--bg-color", value: "#f5f5f5" },
+    { property: "--accent-color", value: "#91ad67" },
+    { property: "--font-color", value: "#333333" },
+    { property: "--tag1-color", value: "#ffe047" },
+    { property: "--tag2-color", value: "#7ab4ff" },
+    { property: "--tag3-color", value: "#dd6b6b" },
+  ],
+};
+
+// Function takes a DOM object, reads dataset.color and value, then changes state accordingly
+export function updateColorTheme(target) {
+  const color = target.dataset.color;
+  const value = target.value;
+  const stateColor = state.colorTheme.find((el) => el.property === color);
+  stateColor.value = value;
+}
