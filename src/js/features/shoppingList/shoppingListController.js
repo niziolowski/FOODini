09ff8c325d0cd9f8c01a0f6222544bdf04a74f0e
niziolowski <krystianpiatkowski92@gmail.com>
2022-10-03 21:18:08 +0200

    Added funcionality: add item to shopping-list

--- src/js/features/shoppingList/shoppingListController.js
+++ src/js/features/shoppingList/shoppingListController.js
@@ -1,7 +1,32 @@
 import shoppingListView from "./shoppingListView.js";
 
+function handleClick(e) {
+  const btn = e.target.closest("button");
+
+  if (!btn) return;
+
+  // Btn "add item"
+  if (btn.classList.contains("shopping-list__btn-add")) {
+    // Render
+    shoppingListView.renderNewItem();
+
+    // Add to state
+  }
+
+  // Btn "delete item"
+
+  if (btn.classList.contains("shopping-list-item__btn-delete")) {
+    shoppingListView.removeItem(btn);
+
+    // Remove from state
+  }
+}
+
 function init() {
   console.log("IMPORT SUCCESFUL: shoppingListController");
+  shoppingListView.addHandlerClick(handleClick);
+
+  shoppingListView.show(); //* For testing only
 }
 
 init();
--- src/js/features/shoppingList/shoppingListView.js
+++ src/js/features/shoppingList/shoppingListView.js
@@ -1,12 +1,19 @@
 class shoppingListView {
   _parentElement = document.querySelector(".shopping-list");
   _btnToggle = document.querySelector(".shopping-list__btn-toggle");
+  _btnAdd = document.querySelector(".shopping-list__btn-add");
+  _content = document.querySelector(".shopping-list-content");
   _status = "inactive";
   constructor() {
     this.init();
   }
   init() {
     this._addHandlerBtnToggle(); // Handle hover and click events on toggle button
+  }
+
+  // For every click other than toggle btn
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener("click", handler.bind(this));
   }
 
   _addHandlerBtnToggle() {
@@ -50,6 +57,50 @@ class shoppingListView {
       default:
         break;
     }
+  }
+
+  // Render empty row for a new item
+  renderNewItem() {
+    // The js-new class is a one time use for focusing on newly created list item
+    const markup = `
+    <li class="shopping-list-item js-new">
+      <input class="shopping-list-item__checkbox" type="checkbox">
+      <input class="shopping-list-item__name" contenteditable="true" placeholder="Produkt">
+      <input type="number" class="shopping-list-item__amount" contenteditable="true" placeholder="1">
+      <select class="shopping-list-item__unit">
+        <option>szt.</option>
+        <option>kg</option>
+        <option>g</option>
+        <option>ml</option>
+      </select>
+      <button class="shopping-list-item__btn-delete btn-icon small">
+        <i data-feather="trash"></i>
+      </button>
+    </li>
+    `;
+
+    // Render new item
+    this._btnAdd.insertAdjacentHTML("beforebegin", markup);
+
+    // get svg icons
+    feather.replace();
+
+    // Get new item El
+    const newItemEl = this._content.querySelector(".js-new");
+
+    // Focus on product input
+    newItemEl.querySelector(".shopping-list-item__name").focus();
+
+    // Remove .js-new class
+    newItemEl.classList.remove("js-new");
+  }
+
+  removeItem(btn) {
+    // Find list item
+    const itemEl = btn.closest("LI");
+
+    // Remove element
+    itemEl.remove();
   }
 
   show() {
