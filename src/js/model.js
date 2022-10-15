90e1eff08ecbb4e9f9b5523a151a159271ee89f4
niziolowski <krystianpiatkowski92@gmail.com>
2022-10-15 18:42:49 +0200

    preparing for contest

--- src/js/model.js
+++ src/js/model.js
@@ -174,12 +174,16 @@ const recipes = [
     name: "Jajecznica",
     group: "śniadanie",
     difficulty: 1,
-    ingredients: [{ name: "Jajka", unit: "szt.", amount: 4 }],
+    ingredients: [
+      { name: "Jajka", unit: "szt.", amount: 4 },
+      { name: "Boczek", unit: "g", amount: 50 },
+    ],
     spices: ["sól", "pieprz"],
     bookmark: false,
     image_url:
       "https://cdn.galleries.smcloud.net/t/photos/gf-bc5p-Ttbo-1SJC_jajecznica-podstawowy-przepis-na-klasyczna-potrawe-z-rozmaconych-jajek.jpg",
-    description: "Normalnie, jajka rozbić na patelnii i smażyć aż się zetno.",
+    description:
+      "Rozgrzać patelnię i podsmażyć boczek pokrojony w kostkę, dodać jajka i oprószyć delikatnie solą. Smażyć na małym ogniu, delikatnie przesuwając jajka drewnianą łyżką. Doprawić świeżo zmielonym czarnym pieprzem.",
   },
   {
     id: 2,
@@ -197,6 +201,45 @@ const recipes = [
       "https://cdn.galleries.smcloud.net/t/photos/gf-Yz2K-uho6-f6NT_spaghetti-bolognese-przepis-na-wloskie-danie-dla-4-osob.jpg",
     description:
       "Makaron gotujemy al dente. Obieramy cebulę i kroimy w kosteczkę, ząbki czosnku drobno siekamy. Parmezan ścieramy na drobniutkich oczkach. \n\n\n      Rozgrzewamy oliwę na głębokiej patelni i podsmażamy do zeszklenia cebulę i czosnek. Na patelnię dodajemy pomidory z puszki (z całą zalewą), łyżeczkę koncentratu i przyprawy.\n      \n      Ugotowany makaron wrzucamy na patelnię z sosem, mieszamy i gotujemy około 1-2 minut.\n      Dodajemy pokrojoną na mniejsze kawałki bazylię, 1 łyżkę startego parmezanu i mieszamy.\n      \n      Podajemy na talerzach posypane resztą parmezanu i przyozdobione świeżymi listkami bazylii.",
+  },
+  {
+    id: 3,
+    created_at: 1664716054736,
+    name: "Stek z pieprzem",
+    group: "obiad",
+    difficulty: 2,
+    ingredients: [
+      { name: "Stek wołowy", unit: "g", amount: 350 },
+      { name: "Masło klarowane", unit: "g", amount: 20 },
+    ],
+    spices: ["sól", "pieprz"],
+    bookmark: true,
+    image_url:
+      "https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/stek_z_pieprzem_1.jpg",
+    description: `Steki ogrzać do temperatury pokojowej. Osuszyć papierowym ręcznikiem, obtoczyć w pieprzu (nie solić). Rozgrzać grubą patelnię (np. żeliwną) na średnim ogniu. Wlać oliwę i gdy będzie gorąca włożyć steki. Smażyć na średnio dużym ogniu przez 4 - 5 minut, aż będą zrumienione i wysmażone mniej więcej do 1/3 wysokości. Przewrócić na drugą stronę i smażyć przez kolejne 4 - 5 minuty. Zdjąć z patelni, położyć na podgrzanym talerzu (talerz można podgrzać na parze kładąc go na garnku po gotujących się brokułach), mięso oprószyć odrobiną soli i przykryć folią aluminiową.
+      Na patelnię po stekach wlać brandy i łopatką zeskrobać to co pozostało na dnie, podgrzewać przez około pół minuty, aż płyn zredukuje się o połowę. Wlać sok ze steków (odstawione i przykryte steki puszczą odrobinę soku) oraz śmietankę. Gdy sos zgęstnieje odstawić patelnię z ognia, dodać musztardę dijon i przelać do miseczki. W razie konieczności doprawić odrobiną soli.`,
+  },
+  {
+    id: 4,
+    created_at: 1664716054736,
+    name: "Leczo wegetarianskie",
+    group: "obiad",
+    difficulty: 2,
+    ingredients: [
+      { name: "Cebula", unit: "szt.", amount: 1 },
+      { name: "Cukinia", unit: "szt.", amount: 1 },
+      { name: "Bataty", unit: "szt.", amount: 1 },
+    ],
+    spices: ["sól", "pieprz"],
+    bookmark: true,
+    image_url:
+      "https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/leczo-wegetarianskie-z-batatami.jpg",
+    description: `W szerokim garnku na oliwie zeszklić pokrojoną cebulę (co chwilę mieszając, ok. 8 minut).
+    Dodać obranego i pokrojonego w kostkę batata i smażyć co chwilę mieszając, przez ok. 5 minut.
+    Dodać przyprawy, wymieszać, wlać bulion i zagotować. Gotować bez przykrycia przez ok. 5 minut co chwilę mieszając.
+    Dodać pokrojoną w kosteczkę paprykę oraz starty czosnek. Gotować przez ok. 2 minuty.
+    Dodać pokrojoną w kosteczkę cukinię a po chwili dodać zmiksowane pomidory lub passatę. Gotować przez ok. 5 minut do miękkości cukinii.
+    Doprawić w razie potrzeby i wymieszać z posiekaną bazylią.`,
   },
 ];
 
